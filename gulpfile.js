8a8a760116bbbd68f4ca1a3f83f601dd74df3bcf
Suleiman Leadbitter <suleiman.leadbitter@gmail.com>
2014-12-13 19:31:41 +0000

    Working on elements for the project - JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp  = require('gulp'),
-    jade  = require('gulp-jade'),
-    prettify = require('gulp-prettify'),
-    livereload  = require('gulp-livereload'),
-    watch = require('gulp-watch')
+var gulp       = require('gulp'),
+    jade       = require('gulp-jade'),
+    prettify   = require('gulp-prettify'),
+    sass       = require('gulp-sass'),
+    uglify     = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    watch      = require('gulp-watch')
     ;
 
-    // Markup Tasks
+  // Markup Tasks
   gulp.task('markup', function() {
     gulp.src('project/*.jade')
     .pipe(jade())
@@ -13,11 +15,39 @@ var gulp  = require('gulp'),
     .pipe(gulp.dest('build/'));
   });
 
+  // gulp.task('sass', function () {
+  //   gulp.src('project/assets/*.scss')
+  //   .pipe(sass())
+  //   .pipe(gulp.dest('build/css'));
+  // });
+
+  // JavaScript Tasks
+  gulp.task('scripts', function(){
+    gulp.src('js/*.js')
+    gulp.src('bower_components/modernizr/modernizr.js')
+    //.pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js/'))
+  });
+
+
+  // Styles Tasks
+  gulp.task('styles', function() {
+    gulp.src('project/assets/styles/*.scss')
+    .pipe(sass({
+      style: 'expanded'
+    }))
+    .pipe(gulp.dest('build/css/'))
+  });
+
+
     // Watch Task
     gulp.task('watch', function() {
       var server = livereload();
       gulp.watch('project/*.jade', ['markup']);
+      gulp.watch('project/assets/*.scss', ['styles']);
     });
 
+
     // Default task to be run with `gulp`
-    gulp.task('default', ['markup','watch']);
+    gulp.task('default', ['markup','styles','watch']);
